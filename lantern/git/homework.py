3de0306cda6c4dbb69dad6a9cba5c231d32343d9
Webmaster <webmaster.us50@gmail.com>
2020-03-12 23:16:14 +0200

    finished files with homework

--- lantern/git/homework.py
+++ lantern/git/homework.py
@@ -1,10 +1,7 @@
-"""
-This is a list of functions that should be completed.
-"""
-
+#This is a list of functions that should be completed.
 from typing import Any
 from typing import List
-
+import string
 
 class OurAwesomeException(Exception):
     pass
@@ -15,7 +12,7 @@ def is_two_object_has_same_value(first: Any, second: Any) -> bool:
     If @first and @second has same value should return True
     In another case should return False
     """
-    pass
+    return first == second
 
 
 def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
@@ -23,48 +20,47 @@ def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
     If @first and @second has same type should return True
     In another case should return False
     """
-    pass
+    return type(first) == type(second)
 
 
 def is_two_objects_is_the_same_objects(first: Any, second: Any) -> bool:
     """
     If @first and @second has same type should return True
     In another case should return False
+    :type first: object
     """
-    pass
+    return first is second
 
 
 def multiple_ints(first_value: int, second_value: int) -> int:
     """
     Should calculate product of all args.
     if first_value or second_value is not int should raise TypeError
-
     Raises:
         TypeError
-
     Params:
         first_value: value for multiply
         second_value
     Returns:
         Product of elements
     """
-    pass
+    try:
+        product_of_elements = first_value * second_value
+        return product_of_elements
+    except TypeError:
+        print("TypeError")
 
 
 def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
     """
     If possible to convert arguments to int value - convert and multiply them.
     If it is impossible raise ValueError
-
     Args:
         first_value: number for multiply
         second_value: number for multiply
-
     Raises:
         ValueError
-
     Returns: multiple of two numbers.
-
     Examples:
         multiple_ints_with_conversion(6, 6)
         >>> 36
@@ -78,33 +74,39 @@ def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
             print("Not valid input data")
         >>> "Not valid input data"
     """
-    pass
+    if int(first_value) == int and int(second_value) == int:
+        multiple_of_two_numbers = first_value * second_value
+        return multiple_of_two_numbers
+    else:
+        raise ValueError
+
 
 
 def is_word_in_text(word: str, text: str) -> bool:
     """
     If text contain word return True
     In another case return False.
-
     Args:
         word: Searchable substring
         text: Text for search
-
     Examples:
         is_word_in_text("Hello", "Hello word")
         >>> True
         is_word_in_text("Glad", "Nice to meet you ")
         >>> False
-
     """
-    pass
+    return word in text
 
 
 def some_loop_exercise() -> list:
     """
     Use loop to create list that contain int values from 0 to 12 except 6 and 7
     """
-    pass
+    list_from_loop = [x for x in range(13)]
+
+    list_from_loop.remove(6)
+    list_from_loop.remove(7)
+    return list_from_loop
 
 
 def remove_from_list_all_negative_numbers(data: List[int]) -> list:
@@ -116,7 +118,13 @@ def remove_from_list_all_negative_numbers(data: List[int]) -> list:
         remove_from_list_all_negative_numbers([1, 5, -7, 8, -1])
         >>> [1, 5, 8]
     """
-    pass
+    list_positiv_number = []
+    for i in data:
+        if i < 0:
+            continue
+        else:
+           list_positiv_number.append(i)
+    return list_positiv_number
 
 
 def alphabet() -> dict:
@@ -127,7 +135,14 @@ def alphabet() -> dict:
         alphabet()
         >>> {"a": 1, "b": 2 ...}
     """
-    pass
+    number = 1
+    result = string.ascii_lowercase
+    dictionary = {}
+
+    for i in result:
+        dictionary[number] = i
+        number += 1
+    return dictionary
 
 
 def simple_sort(data: List[int]) -> List[list]:
@@ -137,4 +152,8 @@ def simple_sort(data: List[int]) -> List[list]:
         simple_sort([2, 9, 6, 7, 3, 2, 1])
         >>> [1, 2, 2, 3, 6, 7, 9]
     """
-    pass
+    for j in range(len(data) - 1):
+        for i in range(len(data) - 1):
+            if data[i] > data[i + 1]:
+                data[i], data[i + 1] = data[i + 1], data[i]
+    return data
